060dee637a7f6a5c14c701bb6266a0e4bb724dc5
Manjiri Ashwin Ambulkar <mambulkar@altimetrik.com>
2022-03-22 19:07:10 +0530

    all data commit

---
+++ src/main/java/com/example/app/CicdDemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdDemo2Application.class, args);
+	}
+
+}
